dcf0f0fbc85ab980aa67904224c715cc75122959
PremRathan2003 <premrathanaddanki7736@gmail.com>
2023-12-10 18:19:47 +0530

    Created BMICalculator component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
 import LandingPage from './components/LandingPage'
+import BMICalculator from './components/BMICalculator'
 
 const App = () => {
   return(
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={LandingPage} />          
+          <Route exact path='/' component={LandingPage} />  
+          <Route path='/bmi-calculator' component={BMICalculator}/>
         </Switch>
       </BrowserRouter>
   )
