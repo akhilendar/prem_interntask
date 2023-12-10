533955b35030091f678f9a1ea065811e0fdac446
PremRathan2003 <premrathanaddanki7736@gmail.com>
2023-12-10 18:11:20 +0530

    Created LandingPage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-
+import LandingPage from './components/LandingPage'
 
 const App = () => {
   return(
       <BrowserRouter>
         <Switch>
-          
+          <Route exact path='/' component={LandingPage} />          
         </Switch>
       </BrowserRouter>
   )
