58de3f644dcd6759083b5645ce9f85782d4ddeae
PremRathan2003 <premrathanaddanki7736@gmail.com>
2023-12-13 17:44:18 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import BMICalculator from './components/BMICalculator'
 
-const App = () => {
-  return(
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={LandingPage} />  
-          <Route path='/bmi-calculator' component={BMICalculator}/>
-        </Switch>
-      </BrowserRouter>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/bmi-calculator" component={BMICalculator} />
+    </Switch>
+  </BrowserRouter>
+)
+
 export default App
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,29 +1,55 @@
-import { Component } from "react";
+import {Component} from 'react'
 
 const candidateDetails = {
-    firstName: "Prem Rathan",
-    highestDegreePassoutYear: "April-2024",
-    technologiesWorkedWith: ['React', 'Python', 'HTML', 'CSS', 'JavaScript', 'SQL', 'NodeJS', 'Bootstrap'],
+  firstName: 'Prem Rathan',
+  highestDegreePassoutYear: 'April-2024',
+  technologiesWorkedWith: [
+    'React',
+    'Python',
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'SQL',
+    'NodeJS',
+    'Bootstrap',
+  ],
 }
 
-class LandingPage extends Component{
-    render(){
-        const downloadResume = () => {
-            window.open('https://drive.google.com/file/d/1m0rrSsW7LyuSVQj75ozSJkax5lc_XPB9/view')
-        }
-
-        return(
-            <div style={{padding: '10px' }}>
-                <h1>Welcome to the Learn React Page</h1>
-                <h1>First Name: {candidateDetails.firstName}</h1>
-                <p>Highest Degree Passout Year: {candidateDetails.highestDegreePassoutYear}</p>
-                <p>Technologies/Languages: {candidateDetails.technologiesWorkedWith.join(', ')}</p>
-                <button onClick={downloadResume}>Download Resume</button>
-                <p>Click here to Calculate your BMI: <a onClick={() => {window.location.href = "/bmi-calculator";}}>
-                    <button>BMI Calculator</button>
-                </a></p>
-            </div>
-        )
+class LandingPage extends Component {
+  render() {
+    const downloadResume = () => {
+      window.open(
+        'https://drive.google.com/file/d/1m0rrSsW7LyuSVQj75ozSJkax5lc_XPB9/view',
+      )
     }
+
+    return (
+      <div style={{padding: '10px'}}>
+        <h1>First Name: {candidateDetails.firstName}</h1>
+        <p>
+          Highest Degree Passout Year:{' '}
+          {candidateDetails.highestDegreePassoutYear}
+        </p>
+        <p>
+          Technologies/Languages:{' '}
+          {candidateDetails.technologiesWorkedWith.join(', ')}
+        </p>
+        <button type="button" onClick={downloadResume}>
+          Download Resume
+        </button>
+        <p>
+          Click here to Calculate your BMI:{' '}
+          <button
+            type="button"
+            onClick={() => {
+              window.location.href = '/bmi-calculator'
+            }}
+          >
+            BMI Calculator
+          </button>
+        </p>
+      </div>
+    )
+  }
 }
 export default LandingPage
