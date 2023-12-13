65984fe4bea1ac1af9f3980d20d4baaf738eace5
PremRathan2003 <premrathanaddanki7736@gmail.com>
2023-12-13 17:14:47 +0530

    deploy

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import LandingPage from './components/LandingPage'
-
-test('renders existing text in LandingPage', () => {
-  render(<LandingPage />);
-  const existingText = screen.getAllByRole(/button/i);
-  expect(existingText).toBeInTheDocument();
-});
